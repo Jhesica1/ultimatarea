535b5b46484f612f40bae42b926198db45ca1d8e
Jhesica1 <145148679+Jhesica1@users.noreply.github.com>
2025-06-15 19:03:27 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    hmr: {
-      overlay: false // Desactiva el overlay de errores HMR si es necesario
-    }
-  }
   base: '/ultimatarea/',
 })
